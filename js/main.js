77c329c8e6070b07610c4a699765877df83f0cce
denev <65989857+deneb0723@users.noreply.github.com>
2021-06-01 20:47:46 +0900

    header, visual 부분 완성

--- js/main.js
+++ js/main.js
@@ -18,3 +18,30 @@ const handleBlur = () => {
 searchEl.addEventListener("click", handleClick);
 searchInputEl.addEventListener("focus", handleFocus);
 searchInputEl.addEventListener("blur", handleBlur);
+
+const badgeEl = document.querySelector("header .badges");
+
+const handleScroll = () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: "none",
+    });
+  } else {
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: "block",
+    });
+  }
+};
+
+window.addEventListener("scroll", _.throttle(handleScroll, 400));
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});

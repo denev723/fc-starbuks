f0cc623109562d65ad4cd75a5e9900b0d6351458
denev <denev723@gmail.com>
2021-06-16 06:55:13 +0900

    seperate common module

---
+++ js/common.js
@@ -0,0 +1,23 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+const handleClick = () => {
+	searchInputEl.focus();
+};
+
+const handleFocus = () => {
+	searchEl.classList.add("focused");
+	searchInputEl.setAttribute("placeholder", "통합검색");
+};
+
+const handleBlur = () => {
+	searchEl.classList.remove("focused");
+	searchInputEl.setAttribute("placeholder", "");
+};
+
+searchEl.addEventListener("click", handleClick);
+searchInputEl.addEventListener("focus", handleFocus);
+searchInputEl.addEventListener("blur", handleBlur);
+
+const thisYear = document.querySelector(".this-year");
+thisYear.innerText = new Date().getFullYear();
